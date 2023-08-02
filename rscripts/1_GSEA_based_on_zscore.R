7ae09590cc18faf5574e5d42008dcdf2f19f0d00
Iggi-29 <ignasijs29@gmail.com>
2023-08-02 12:53:54 +0200

    GSEA based on zscore and percentile (optional)

--- rscripts/1_GSEA_based_on_zscore.R
+++ rscripts/1_GSEA_based_on_zscore.R
@@ -156,7 +156,35 @@ zscore_pac2 <- expression_matrix_zscore[expression_matrix_zscore$pacient_group =
 zscore_pac3 <- expression_matrix_zscore[expression_matrix_zscore$pacient_group == "3",]
 zscore_pac4 <- expression_matrix_zscore[expression_matrix_zscore$pacient_group == "4",]
 
-#### ADD A WAY TO FILTER OUT GENES !!!!!! <- SPEAK THIS TOMORROW !!!!
+# Absolute zscore value for percentile calculation
+zscore_k1$zscore_abs <- abs(zscore_k1$zscore)
+zscore_k2$zscore_abs <- abs(zscore_k2$zscore)
+zscore_k3$zscore_abs <- abs(zscore_k3$zscore)
+zscore_k4$zscore_abs <- abs(zscore_k4$zscore)
+zscore_k5$zscore_abs <- abs(zscore_k5$zscore)
+zscore_k6$zscore_abs <- abs(zscore_k6$zscore)
+
+# Percentile based on abolute zscore value
+zscore_k1$zscore_per <- percent_rank(zscore_k1$zscore_abs)
+zscore_k2$zscore_per <- percent_rank(zscore_k2$zscore_abs)
+zscore_k3$zscore_per <- percent_rank(zscore_k3$zscore_abs)
+zscore_k4$zscore_per <- percent_rank(zscore_k4$zscore_abs)
+zscore_k5$zscore_per <- percent_rank(zscore_k5$zscore_abs)
+zscore_k6$zscore_per <- percent_rank(zscore_k6$zscore_abs)
+
+# Filter by percentile
+zscore_k1 <- zscore_k1 %>% 
+  filter(zscore_per > 0.5)
+zscore_k2 <- zscore_k2 %>% 
+  filter(zscore_per > 0.5)
+zscore_k3 <- zscore_k3 %>% 
+  filter(zscore_per > 0.5)
+zscore_k4 <- zscore_k4 %>% 
+  filter(zscore_per > 0.5)
+zscore_k5 <- zscore_k5 %>% 
+  filter(zscore_per > 0.5)
+zscore_k6 <- zscore_k6 %>% 
+  filter(zscore_per > 0.5)
 
 # Create a named vector and order it by decreasing order
 z_score_value_pac1 <- zscore_pac1$zscore
