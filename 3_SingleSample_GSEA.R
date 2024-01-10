591d76579eaf1376f88a9dfe8405ccba8c6322f1
Iggi-29 <ignasijs29@gmail.com>
2024-01-10 11:26:08 +0100

    the code did not realy change

--- 3_SingleSample_GSEA.R
+++ 3_SingleSample_GSEA.R
@@ -14,12 +14,12 @@ data(c2BroadSets)
 class(c2BroadSets)
 data(gbm_VerhaakEtAl)
 expression_matrix <- exprs(gbm_eset)
-expression_matrix <- expression_matrix[,c(1:2)]
+expression_matrix <- expression_matrix[,c(1:32)]
 
 data(brainTxDbSets)
 pathways <- as.list(brainTxDbSets)
 pathways <- fgsea::gmtPathways("./raw_data/Human_GOBP_AllPathways_no_GO_iea_July_03_2023_symbol.gmt")
-pathways <- pathways[1:4000]
+pathways <- pathways[1:1000]
 
 remove(c2BroadSets)
 remove(gbm_eset)
@@ -27,9 +27,9 @@ remove(brainTxDbSets)
 
 tictoc::tic()
 result <- ssGSEA(expression_matrix = expression_matrix, pathways = pathways,  
-                 max_length = 500, min_length = 30, min_Ratio = 0,
+                 max_length = 500, min_length = 30, min_Ratio = 0.66,
                  organism = "org.Hs.eg.db", use_enrichr = T, 
-                 collapse = F,
+                 collapse = T,
                  alpha = 0.25, scale_size = F, scale_ratio = F, curated_results = T)
 tictoc::toc()
 
