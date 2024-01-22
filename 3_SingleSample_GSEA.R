bb521e653cc59d438f4a8a701ea38e1271eb70fd
Iggi-29 <ignasijs29@gmail.com>
2024-01-22 15:48:51 +0100

    RDS file saving

--- 3_SingleSample_GSEA.R
+++ 3_SingleSample_GSEA.R
@@ -10,6 +10,7 @@ library(dplyr)
 ### source the function
 source("./functions/ssGSEA.R")
 
+### Import some data expression and pathway data expression
 data(c2BroadSets)
 class(c2BroadSets)
 data(gbm_VerhaakEtAl)
@@ -19,15 +20,31 @@ expression_matrix <- expression_matrix[,c(1:2), drop = F]
 data(brainTxDbSets)
 pathways <- as.list(brainTxDbSets)
 pathways <- fgsea::gmtPathways("./raw_data/Human_GOBP_AllPathways_no_GO_iea_July_03_2023_symbol.gmt")
-pathways <- pathways[1:400]
+pathways <- pathways[1:4000]
 
 remove(c2BroadSets)
 remove(gbm_eset)
 remove(brainTxDbSets)
 
+### Execute the function
 library(org.Hs.eg.db) #### In order that the enrichment works the library has to be loaded onto to environment <3
 tictoc::tic()
-result <- ssGSEA(expression_matrix = expression_matrix, pathways = pathways, recycle = F,
+result_1 <- ssGSEA(expression_matrix = expression_matrix, pathways = pathways, recycle = F,
+                 max_length = 100, min_length = 10, min_Ratio = 0,
+                 organism = "org.Hs.eg.db", use_enrichr = F, 
+                 collapse = F, 
+                 alpha = 0.25, scale_size = F, scale_ratio = F, curated_results = F)
+tictoc::toc()
+
+
+### Save the enriched path list
+saveRDS(pathways_enriched, file = "./results/path_list_max100_min10_0ratio.rds")
+
+### Load enriched path list
+pathways_onto_the_analysis <- readRDS("./results/path_list_max100_min10_0ratio.rds")
+
+tictoc::tic()
+result_2 <- ssGSEA(expression_matrix = expression_matrix, pathways = pathways, recycle = T,
                  max_length = 100, min_length = 10, min_Ratio = 0,
                  organism = "org.Hs.eg.db", use_enrichr = F, 
                  collapse = F, 
